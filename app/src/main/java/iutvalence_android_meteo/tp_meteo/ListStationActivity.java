d97f1898b952ff53f54a829173a6d38207c842f8
partulaj <jeremie.partula@outlook.fr>
2016-02-04 22:25:02 +0100

    Première Intent

---
+++ app/src/main/java/iutvalence_android_meteo/tp_meteo/ListStationActivity.java
@@ -0,0 +1,13 @@
+package iutvalence_android_meteo.tp_meteo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListStationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_station);
+    }
+}
