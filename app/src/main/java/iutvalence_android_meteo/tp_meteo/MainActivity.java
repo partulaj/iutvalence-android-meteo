3efae7d72dffb9f047cd50d094458fcb88032c54
QtoR <breton.que@gmail.com>
2016-02-06 16:02:10 +0100

    Début TP Meteo - première version du layout du mainactivity faite

--- app/src/main/java/iutvalence_android_meteo/tp_meteo/MainActivity.java
+++ app/src/main/java/iutvalence_android_meteo/tp_meteo/MainActivity.java
@@ -1,16 +1,27 @@
 package iutvalence_android_meteo.tp_meteo;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import org.json.JSONObject;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import worker.JSONParser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private JSONParser jsonParser = new JSONParser();
+    private static final String GET_ONE_STATION = "http://intranet.iut-valence.fr/~partulaj/MesTPs/Casir/TP-Meteo/controller/RequestController.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +29,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Récupération d'une station
+        String test = null;
+        try {
+            test = new getUser().execute().get();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void toListStationActivity(View view) {
@@ -45,5 +68,19 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public class getUser extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... args) {
+            List<String> params = Arrays.asList("id");
+            List<String> values = Arrays.asList("Montélimar");
+            JSONObject json = jsonParser.makeHttpRequest(
+                    GET_ONE_STATION, params, values);
+            Log.d("getOneStation", json.toString());
+            return json.toString();
+        }
+
     }
 }
--- app/src/main/java/worker/JSONParser.java
+++ app/src/main/java/worker/JSONParser.java
@@ -27,10 +27,10 @@ import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.List;
-
-//ANCIENNES METHODES - DEPRECIES
 
 public class JSONParser {
     static InputStream is = null;
@@ -60,9 +60,6 @@ public class JSONParser {
             conn.setRequestProperty("Content-Type", "application/json");
             conn.setRequestProperty("Accept", "application/json");
             conn.connect();
-
-            InputStream is = conn.getInputStream();
-
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
@@ -103,105 +100,39 @@ public class JSONParser {
         return jObj;
     }
 
-
-    //ANCIENNE METHODE - Déprécié
-
-    /*public JSONObject getJSONFromUrl(final String url) {
-
-        // Making HTTP request
+    public JSONObject makeHttpRequest(String url, List<String> params, List<String> values) {
+        URL monURL;
+        String query = null;
+        HttpURLConnection conn = null;
         try {
-            // Construct the client and the HTTP request.
-            DefaultHttpClient httpClient = new DefaultHttpClient();
-            HttpPost httpPost = new HttpPost(url);
+            for (int i = 0; i < params.size(); i++) {
+                query = String.format(params.get(i) + "=%s", URLEncoder.encode(values.get(i), "UTF-8"));
+            }
+            monURL = new URL(url + "?" + query);
+            Log.i("url", monURL.toString());
+            conn = (HttpURLConnection) monURL.openConnection();
 
-            // Execute the POST request and store the response locally.
-            HttpResponse httpResponse = httpClient.execute(httpPost);
-            // Extract data from the response.
-            HttpEntity httpEntity = httpResponse.getEntity();
-            // Open an inputStream with the data content.
-            is = httpEntity.getContent();
+            String infosCompte = "bretonq:wv5qR8qv";
+            String encoded = Base64.encodeToString(infosCompte.getBytes(), Base64.DEFAULT);
 
-        } catch (UnsupportedEncodingException e) {
+            conn.setRequestProperty("Authorization", "basic " + encoded);
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept-Charset", "utf-8");
+
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.connect();
+
+            is = conn.getInputStream();
+            Log.d("content", is.toString());
+        } catch (MalformedURLException e) {
             e.printStackTrace();
-        } catch (ClientProtocolException e) {
+        } catch (ProtocolException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        try {
-            // Create a BufferedReader to parse through the inputStream.
-            BufferedReader reader = new BufferedReader(new InputStreamReader(
-                    is, "iso-8859-1"), 8);
-            // Declare a string builder to help with the parsing.
-            StringBuilder sb = new StringBuilder();
-            // Declare a string to store the JSON object data in string form.
-            String line = null;
-
-            // Build the string until null.
-            while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
-            }
-
-            // Close the input stream.
-            is.close();
-            // Convert the string builder data to an actual string.
-            json = sb.toString();
-        } catch (Exception e) {
-            Log.e("Buffer Error", "Error converting result " + e.toString());
-        }
-
-        // Try to parse the string to a JSON object
-        try {
-            jObj = new JSONObject(json);
-        } catch (JSONException e) {
-            Log.e("JSON Parser", "Error parsing data " + e.toString());
-        }
-
-        // Return the JSON Object.
-        return jObj;
-    }
-    */
-
-    // function get json from url
-    // by making HTTP POST or GET mehtod
-    public JSONObject makeHttpRequest(String url, String method,
-                                      List<NameValuePair> params) {
-
-        // Making HTTP request
-        try {
-
-            // check for request method
-            if (method == "POST") {
-                // request method is POST
-                // defaultHttpClient
-                DefaultHttpClient httpClient = new DefaultHttpClient();
-                HttpPost httpPost = new HttpPost(url);
-                httpPost.setEntity(new UrlEncodedFormEntity(params));
-
-                HttpResponse httpResponse = httpClient.execute(httpPost);
-                HttpEntity httpEntity = httpResponse.getEntity();
-                is = httpEntity.getContent();
-
-            } else if (method == "GET") {
-                // request method is GET
-                DefaultHttpClient httpClient = new DefaultHttpClient();
-                String paramString = URLEncodedUtils.format(params, "utf-8");
-                url += "?" + paramString;
-                HttpGet httpGet = new HttpGet(url);
-
-                HttpResponse httpResponse = httpClient.execute(httpGet);
-                HttpEntity httpEntity = httpResponse.getEntity();
-                is = httpEntity.getContent();
-            }
-
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(
@@ -226,7 +157,10 @@ public class JSONParser {
 
         // return JSON String
         return jObj;
+
     }
+
+    //ANCIENNES METHODES - JSONArray
 
     public JSONArray getJSONArrayFromUrl(final String url) {
         // Making HTTP request
